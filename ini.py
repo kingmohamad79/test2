fb7d789c704de0da2bb0a4d500cfe1a337efa691
mohammad nouri <norimohamad115@gmail.com>
2024-03-31 11:14:40 +0330

    second commit

--- ini.py
+++ ini.py
@@ -1,6 +1,13 @@
 class Human:
-    pass
+    number_of_legs = 2 # class atribute
+    number_of_hands = 2 # class atribute
 
-mohammad = Human()
+    def __init__(self,age,education) -> None: # constructor function
+        self.age = age # instance atribute
+        self.education = education # instance atribute
 
-print(mohammad)
+mohammad = Human(23,'bachelor')
+ali = Human(25, "diploma")
+
+print(mohammad.age)
+print(ali.age)
